6894339e012bf30fd7ae98b82bdd382264bd344d
Ricardo Espirito Santo <ricardo.santo@evrythng.com>
2018-06-28 18:10:53 +0100

    Adding a the scafold for the project for details check: https://github.com/spring-io/initializr

---
+++ src/main/java/com/ricardoespsanto/gupgop/GupgopApplication.java
@@ -0,0 +1,12 @@
+package com.ricardoespsanto.gupgop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GupgopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GupgopApplication.class, args);
+  }
+}
---
+++ src/test/java/com/ricardoespsanto/gupgop/GupgopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ricardoespsanto.gupgop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GupgopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
